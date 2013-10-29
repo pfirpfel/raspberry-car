2033a3ab225f84db01b6aaade4f6295c7cb98bbc
Michael Kuenzli <michaelkuenzli@gmail.com>
2013-10-29 14:25:55 +0100

    added client lock

--- remote-controller.js
+++ remote-controller.js
@@ -4,6 +4,10 @@ var app = require('http').createServer(handler)
 
 app.listen(5000);
 
+/*
+TODO: serve all files (express?)
+*/
+
 function handler (req, res) {
   fs.readFile(__dirname + '/html/index.html',
   function (err, data) {
@@ -11,14 +15,33 @@ function handler (req, res) {
       res.writeHead(500);
       return res.end('Error loading index.html');
     }
-
     res.writeHead(200);
     res.end(data);
   });
 }
 
+var client_lock = null;
+
 io.sockets.on('connection', function (socket) {
+  if(client_lock === null){
+	client_lock = socket.id;
+	console.log('accepted client: ' + client_lock);
+  } else {
+	console.log('denied client: ' + client_lock);
+  }
   socket.on('control_pressed', function(data){
-	console.log(data);
+	if(client_lock === socket.id){
+		console.log('client: ' + socket.id + ' : ' +data);
+	} else {
+		console.log('command ignored from: ' + socket.id);
+	}
+  });
+  socket.on('disconnect', function(data){
+	if(client_lock === socket.id){
+		client_lock = null;
+		console.log('lock freed: ' + socket.id);
+	} else {
+		console.log('denied client disconnected: ' + socket.id);
+	}
   });
 });
